2db99ead2a87309ee6340e002ef548e394beeb1b
zhouzy <781328783@qq.com>
2017-11-13 18:25:49 +0800

    百度地图测试Demo

---
+++ app/src/main/java/cn/zhouzy/baidumapdemo/MainActivity.java
@@ -0,0 +1,44 @@
+package cn.zhouzy.baidumapdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.offline.MKOfflineMap;
+import com.baidu.mapapi.map.offline.MKOfflineMapListener;
+
+public class MainActivity extends Activity {
+    /**
+     * 百度地图View
+     */
+    private MapView mMapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.mian_mapview_map);
+//        MKOfflineMap mOffline = new MKOfflineMap();
+//        mOffline.init(this);// 传入接口事件，离线地图更新会触发该回调
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+}
